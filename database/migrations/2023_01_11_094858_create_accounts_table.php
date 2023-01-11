4ceedc66388a6d1ea3f2629d55f82b08b8070271
Arslan23 <expeditaza@gmail.com>
2023-01-11 18:22:02 +0100

    Saving

---
+++ database/migrations/2023_01_11_094858_create_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       /* Schema::create('accounts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->decimal('amount', $precision = 8, $scale = 2);
+            $table->timestamps();
+        });*/
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
